337f42c56c514fc4d6271b48f1f8c38a8502b0ff
MarcoBA12 <alegriamarcoalegria@gmail.com>
2023-05-07 11:04:58 -0500

    DAWI_CL1_BegazoMarco

---
+++ src/main/java/pe/edu/cibertec/examen/DawiCl1MarcoBegazoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl1MarcoBegazoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl1MarcoBegazoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/examen/DawiCl1MarcoBegazoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl1MarcoBegazoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
